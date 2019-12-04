32e8fe968f46d4a2ba37082c8960250e0c011a7a
Prajakta Lahankar <46910502+lahankarprajakta@users.noreply.github.com>
2019-12-04 16:39:16 -0500

    Footer

---
+++ footer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Prajakta
+ * Date: 5/15/2019
+ * Time: 6:02 PM
+ */
+
+?>
+
+
+
+<footer>
+<!-- Copywrite Area -->
+<div class="container">
+    <div class="copywrite-content">
+        <div class="row">
+            <!-- Copywrite Text -->
+            <div class="col-12 col-md-6">
+                <div class="copywrite-text">
+                    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                </div>
+            </div>
+            <!-- Footer Menu -->
+            <div class="col-12 col-md-6">
+                <div class="footer-menu">
+                    <ul class="nav">
+                        <li><a href="#"><i class="zmdi zmdi-circle"></i> Terms of Service</a></li>
+                        <li><a href="#"><i class="zmdi zmdi-circle"></i> Privacy Policy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
+<!-- Footer Area End -->
+
+<!-- **** All JS Files ***** -->
+<!-- jQuery 2.2.4 -->
+<script src="js/jquery.min.js"></script>
+<!-- Popper -->
+<script src="js/popper.min.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- All Plugins -->
+<script src="js/confer.bundle.js"></script>
+<!-- Active -->
+<script src="js/default-assets/active.js"></script>
+
+</body>
+
+</html>
