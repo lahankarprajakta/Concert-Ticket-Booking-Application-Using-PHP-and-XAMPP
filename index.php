7b32f238322add991815915282c3157d41ecf498
Prajakta Lahankar <46910502+lahankarprajakta@users.noreply.github.com>
2019-12-04 16:37:50 -0500

    Index

---
+++ index.php
@@ -0,0 +1,152 @@
+<?php
+require_once("config.php");
+require_once("header.php");
+require_once("left-nav.php");
+?>
+
+
+    <!-- Title -->
+    <title>Confer - Conference Event HTML Template</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="./img/core-img/favicon.png">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+
+
+
+
+
+<section class="welcome-area">
+    <div class="welcome-slides owl-carousel">
+        <!-- Single Slide -->
+
+        <div class="single-welcome-slide bg-img bg-overlay jarallax" style="background-image: url(img/bg-img/1.jpg);">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                    <!-- Welcome Text -->
+                    <div class="col-12">
+                        <div class="welcome-text text-right">
+                            <h2 data-animation="fadeInUp" data-delay="300ms">EventGO</h2>
+                            <h6 data-animation="fadeInUp" data-delay="500ms">NYC</h6>
+                            <div class="hero-btn-group" data-animation="fadeInUp" data-delay="700ms">
+                                <a href="#" class="btn confer-btn">More Information <i class="zmdi zmdi-long-arrow-right"></i></a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Single Slide -->
+        <div class="single-welcome-slide bg-img bg-overlay jarallax" style="background-image: url(img/bg-img/45.jpg);">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                    <!-- Welcome Text -->
+                    <div class="col-12">
+                        <div class="welcome-text-two text-center">
+                            <h5 data-animation="fadeInUp" data-delay="100ms">The event consistently</h5>
+                            <h2 data-animation="fadeInUp" data-delay="300ms">OSHA Compliance</h2>
+                            <!-- Event Meta -->
+                            <div class="event-meta" data-animation="fadeInUp" data-delay="500ms">
+                                <a class="event-date" href="#"><i class="zmdi zmdi-account"></i> January 14, 2019</a>
+                                <a class="event-author" href="#"><i class="zmdi zmdi-alarm-check"></i> Laura Green</a>
+                            </div>
+                            <div class="hero-btn-group" data-animation="fadeInUp" data-delay="700ms">
+                                <a href="#" class="btn confer-btn m-2">View more <i class="zmdi zmdi-long-arrow-right"></i></a>
+                                <a href="#" class="btn confer-btn m-2">Get Tickets <i class="zmdi zmdi-long-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Blog Area Start -->
+    <section class="confer-blog-details-area section-padding-100-0">
+        <div class="container">
+            <div class="row justify-content-center">
+                <!-- Single Post Details Area -->
+                <div class="col-12 col-lg-8 col-xl-9">
+                    <div class="pr-lg-4 mb-100">
+                        <!-- Post Content -->
+                        <div class="post-details-content">
+
+                            <!-- Post Title -->
+                            <h4 class="post-title">EVENTGO</h4>
+
+
+                           <div id='wrapper'>
+                                <div id='content'>
+                                    <h1>EventGo</h1>
+                                  <!--  <div id='left-nav.php'>-->
+
+
+
+
+
+                                    <div id='main'>
+
+                                        <p>Sign Up for Free Concerts!!!</p>
+                                        <p>New Events Coming up... </p>
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                  <html>
+
+                    <footer>
+                        <!-- Copywrite Area -->
+                        <div class="container">
+                            <div class="copywrite-content">
+                                <div class="row">
+                                    <!-- Copywrite Text -->
+                                    <div class="col-12 col-md-6">
+                                        <div class="copywrite-text">
+                                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                                Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                                        </div>
+                                    </div>
+                                    <!-- Footer Menu -->
+                                    <div class="col-12 col-md-6">
+                                        <div class="footer-menu">
+                                            <ul class="nav">
+                                                <li><a href="#"><i class="zmdi zmdi-circle"></i> Terms of Service</a></li>
+                                                <li><a href="#"><i class="zmdi zmdi-circle"></i> Privacy Policy</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
+                    <!-- Footer Area End -->
+
+                    <!-- **** All JS Files ***** -->
+                    <!-- jQuery 2.2.4 -->
+                    <script src="js/jquery.min.js"></script>
+                    <!-- Popper -->
+                    <script src="js/popper.min.js"></script>
+                    <!-- Bootstrap -->
+                    <script src="js/bootstrap.min.js"></script>
+                    <!-- All Plugins -->
+                    <script src="js/confer.bundle.js"></script>
+                    <!-- Active -->
+                    <script src="js/default-assets/active.js"></script>
+
+                    </body>
+
+                    </html>
+
